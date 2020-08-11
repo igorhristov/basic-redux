8427ae8e613ff899f909eda4e67c19d59d9cc95a
igorhristov <igorhristov@sourcemxacademy.mk>
2020-08-11 23:17:35 +0200

    inital create react app for learning redux
    counter class and hooks

---
+++ src/components/HooksCounter/HooksCounter.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './counter.css';
+
+const HooksCounter = () => {
+    const [count, setCount] = useState(0);
+
+    const decrament = () => {
+        setCount(count - 1);
+    };
+    
+    return (
+        <div className='container'>
+            <h2>Hooks Counter</h2>
+
+            <div className='counter'>
+                <button onClick={decrament}>-</button>
+                <span className='count'>{count}</span>
+                <button onClick={() => setCount(count + 1)}>+</button>
+            </div>
+        </div>
+    );
+    {
+    }
+};
+
+export default HooksCounter;
