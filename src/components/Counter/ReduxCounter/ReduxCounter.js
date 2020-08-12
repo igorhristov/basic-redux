88ec2e57f014ef303204385f2747d8bce784847c
igorhristov <igorhristov@sourcemxacademy.mk>
2020-08-12 21:42:23 +0200

    move redux code of counter to counter folder

--- src/components/Counter/ReduxCounter/ReduxCounter.js
+++ src/components/Counter/ReduxCounter/ReduxCounter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { increment, decrement } from '../../../Redux/actions';
+import { increment, decrement } from './Redux/actions';
 
 import '../counter.css';
 
